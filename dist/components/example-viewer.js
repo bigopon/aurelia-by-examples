41612da68680a32fc42411333c49d693e8224698
bigopon <bigopon.777@gmail.com>
2021-07-08 20:54:10 +1000

    chore(app): use esm dist

--- dist/components/example-viewer.js
+++ dist/components/example-viewer.js
@@ -244,12 +244,12 @@ function createImportMap(packages) {
             all[pkg] = `https://unpkg.com/${isAureliaPkg(pkg) ? `${pkg}@dev` : pkg}`;
             return all;
         }, {}), {
-            "@aurelia/kernel": "https://unpkg.com/@aurelia/kernel@dev/dist/bundle/index.js",
-            "@aurelia/runtime": "https://unpkg.com/@aurelia/runtime@dev/dist/bundle/index.js",
-            "@aurelia/runtime-html": "https://unpkg.com/@aurelia/runtime-html@dev/dist/bundle/index.js",
-            "@aurelia/platform": "https://unpkg.com/@aurelia/platform@dev/dist/bundle/index.js",
-            "@aurelia/platform-browser": "https://unpkg.com/@aurelia/platform-browser@dev/dist/bundle/index.js",
-            "@aurelia/metadata": "https://unpkg.com/@aurelia/metadata@dev/dist/bundle/index.js",
+            "@aurelia/kernel": "https://unpkg.com/@aurelia/kernel@dev/dist/esm/index.js",
+            "@aurelia/runtime": "https://unpkg.com/@aurelia/runtime@dev/dist/esm/index.js",
+            "@aurelia/runtime-html": "https://unpkg.com/@aurelia/runtime-html@dev/dist/esm/index.js",
+            "@aurelia/platform": "https://unpkg.com/@aurelia/platform@dev/dist/esm/index.js",
+            "@aurelia/platform-browser": "https://unpkg.com/@aurelia/platform-browser@dev/dist/esm/index.js",
+            "@aurelia/metadata": "https://unpkg.com/@aurelia/metadata@dev/dist/esm/index.js",
         }),
     }, undefined, 2);
 }
